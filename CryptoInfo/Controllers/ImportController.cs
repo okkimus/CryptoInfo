d1c3245fc75f143b37f4a168a13431de987eddc6
okkimus <misame@utu.fi>
2021-11-02 15:08:32 +0200

    Replaced initial React app with TS version

---
+++ CryptoInfo/Controllers/ImportController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoInfo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImportController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> Transactions(IFormFile file)
+        {
+            string fileContents;
+            using (var stream = file.OpenReadStream())
+            using (var reader = new StreamReader(stream))
+            {
+                fileContents = await reader.ReadToEndAsync();
+            }
+            return Ok();
+        }
+        
+        public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size });
+        }
+    }
+}
